a7f69b2d351e96776fbf5150ec49a75990caf7f9
Carmen Salinas <31458839+carmensalinas@users.noreply.github.com>
2021-04-20 01:04:01 -0500

    final version

--- ChallengeCarmenSalinas/src/test/java/AutomatedChallenge.java
+++ ChallengeCarmenSalinas/src/test/java/AutomatedChallenge.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AutomatedChallenge {
 	@Test
 	public void testRater() {
@@ -12,6 +13,7 @@ public class AutomatedChallenge {
 		driver.findElement(By.cssSelector("input[name='user-submitted-email']")).sendKeys("carmen12.salinas@gmail.com");
 		driver.findElement(By.xpath("//input[@name='user-submitted-title']")).sendKeys("Producto Prueba");
 		driver.findElement(By.name("user-submitted-url[]")).sendKeys("http://www.productoprueba.com/");
-		driver.findElement(By.name("user-submitted-content")).sendKeys("Esta es la descripción del producto de prueba.");
+		driver.findElement(By.name("user-submitted-content"))
+				.sendKeys("Esta es la descripcion del producto de prueba.");
 	}
 }
